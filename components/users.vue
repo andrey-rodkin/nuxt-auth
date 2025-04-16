db6caefef2f1013c6b125ae2707adad4a377df48
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 22:02:56 +0300

    feat(front): фильтрация по статусу

--- components/users.vue
+++ components/users.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
 import type { TableColumn } from '#ui/components/Table.vue'
 
+type Status ='active' | 'inactive'
+
 type User = {
   id: string,
   name: string,
   surname: string,
   email: string,
-  status: 'active' | 'inactive',
+  status: Status,
   created: string
 }
 
 const users: User[] = await $fetch('/api/users', {
   method: 'GET',
 })
+
+const statuses = [...new Set(users.map(({status}) => status.toUpperCase()))]
 
 const UBadge = resolveComponent('UBadge')
 
@@ -42,10 +46,11 @@ const columns: TableColumn<User>[] = [
         inactive: 'error' as const
       }[row.getValue('status') as string]
 
-      return h(UBadge, { class: 'capitalize', variant: 'subtle', color }, () =>
+      return h(UBadge, { class: 'uppercase', variant: 'subtle', color }, () =>
           row.getValue('status')
       )
-    }
+    },
+    filterFn: 'equalsString'
   },
   {
     accessorKey: 'created',
@@ -53,8 +58,32 @@ const columns: TableColumn<User>[] = [
   }
 ]
 
+const table = useTemplateRef('table')
+
+const columnFilters = ref([
+  {
+    id: 'status',
+    value: ''
+  }
+])
+
 </script>
 
 <template>
-  <UTable :data="users" :columns="columns"/>
+  <div class="p-4">
+    <div class="flex gap-4">
+      <USelect
+          :model-value="table?.tableApi?.getColumn('status')?.getFilterValue() as Status | undefined"
+          :items="statuses"
+          @update:model-value="table?.tableApi?.getColumn('status')?.setFilterValue($event)"
+          class="w-48 capitalize" />
+
+      <UButton
+          @click="table?.tableApi?.getColumn('status')?.setFilterValue('')"
+          :variant="'ghost'"
+      >Очистить</UButton>
+    </div>
+
+    <UTable ref="table" :data="users" :columns="columns" v-model:column-filters="columnFilters"/>
+  </div>
 </template>
