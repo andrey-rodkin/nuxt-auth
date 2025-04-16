2f57881e8f38bbe3350ac1eb907cbab8234cb1ef
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 20:17:38 +0300

    refactor(front): компонент заголовка

---
+++ components/header.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { user, clear: clearSession } = useUserSession()
+
+async function logout() {
+  await clearSession()
+  await navigateTo('/login')
+}
+</script>
+
+<template>
+  <div v-if="user" class="flex space-y-4 p-4 items-center justify-between">
+    <h1 class="m-0">Добро пожаловать, {{ user.name }}!</h1>
+    <UButton @click="logout">Logout</UButton>
+  </div>
+</template>
