86823999cf71245c05ec4949a917448d18f82fc3
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 20:17:38 +0300

    refactor(front): компонент заголовка

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,8 @@
 definePageMeta({
   middleware: ['auth'],
 })
-
-const { user, clear: clearSession } = useUserSession()
-
-async function logout() {
-  await clearSession()
-  await navigateTo('/login')
-}
 </script>
 
 <template>
-  <div v-if="user">
-    <h1>Welcome {{ user.name }}</h1>
-    <UButton @click="logout">Logout</UButton>
-  </div>
+  <Header />
 </template>
