a779b4419edfd85b10d672602bcaa68e087955e2
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 19:35:25 +0300

    feat(front): форма логина

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ['auth'],
+})
+
+const { user, clear: clearSession } = useUserSession()
+
+async function logout() {
+  await clearSession()
+  await navigateTo('/login')
+}
+</script>
+
+<template>
+  <div v-if="user">
+    <h1>Welcome {{ user.name }}</h1>
+    <UButton @click="logout">Logout</UButton>
+  </div>
+</template>
