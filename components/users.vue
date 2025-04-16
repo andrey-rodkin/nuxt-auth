b09263dbedc41105c550a667c56a03fb53df24cf
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 21:13:53 +0300

    refactor(front): изменены названия столбцов

--- components/users.vue
+++ components/users.vue
@@ -1,10 +1,47 @@
 <script setup lang="ts">
-const users = await $fetch('/api/users', {
+import type { TableColumn } from '#ui/components/Table.vue'
+
+type User = {
+  id: string,
+  name: string,
+  surname: string,
+  email: string,
+  status: 'active' | 'inactive',
+  created: string
+}
+
+const users: User[] = await $fetch('/api/users', {
   method: 'GET',
 })
+const columns: TableColumn<User>[] = [
+  {
+    accessorKey: 'id',
+    header: '#',
+  },
+  {
+    accessorKey: 'name',
+    header: 'Имя',
+  },
+  {
+    accessorKey: 'surname',
+    header: 'Фамилия',
+  },
+  {
+    accessorKey: 'email',
+    header: 'E-mail',
+  },
+  {
+    accessorKey: 'status',
+    header: 'Статус',
+  },
+  {
+    accessorKey: 'created',
+    header: 'Дата регистрации',
+  }
+]
 
 </script>
 
 <template>
-  <UTable :data="users" />
+  <UTable :data="users" :columns="columns"/>
 </template>
