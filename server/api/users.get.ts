c5ee6a22b31c68438f4b85de4cb7dcf92abd1fbc
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-16 21:08:00 +0300

    feat(back): добавлен email в данные пользователей

--- server/api/users.get.ts
+++ server/api/users.get.ts
@@ -3,5 +3,5 @@ import users from './users.json'
 export default defineEventHandler(async (event) => {
     await requireUserSession(event)
 
-    return users.map(({credentials, _comment, ...user}) => user)
+    return users.map(({credentials: {username: email}, _comment, ...user}) => ({...user, email: email}))
 })
