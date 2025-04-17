65a052b81a3bc6d331371656c1a36066cdb44e74
Andrey Rodkin <andrey.rodkin@gmail.com>
2025-04-17 07:27:33 +0300

    feat(front): фильтрация по email с мультивыбором

--- components/users.vue
+++ components/users.vue
@@ -17,27 +17,34 @@ const users: User[] = await $fetch('/api/users', {
 })
 
 const statuses = [...new Set(users.map(({status}) => status.toUpperCase()))]
+const emails = users.map(({email}) => email)
 
 const UBadge = resolveComponent('UBadge')
 
 const columns: TableColumn<User>[] = [
   {
+    id: 'id',
     accessorKey: 'id',
     header: '#',
   },
   {
+    id: 'name',
     accessorKey: 'name',
     header: 'Имя',
   },
   {
+    id: 'surname',
     accessorKey: 'surname',
     header: 'Фамилия',
   },
   {
+    id: 'email',
     accessorKey: 'email',
     header: 'E-mail',
+    filterFn: 'arrIncludesSome'
   },
   {
+    id: 'status',
     accessorKey: 'status',
     header: 'Статус',
     cell: ({ row }) => {
@@ -53,6 +60,7 @@ const columns: TableColumn<User>[] = [
     filterFn: 'equalsString'
   },
   {
+    id: 'created',
     accessorKey: 'created',
     header: 'Дата регистрации',
   }
@@ -64,8 +72,22 @@ const columnFilters = ref([
   {
     id: 'status',
     value: ''
+  },
+  {
+    id: 'email',
+    value: []
   }
 ])
+
+const clearFilters = () => {
+  table?.value?.tableApi?.getColumn('status')?.setFilterValue('')
+  table?.value?.tableApi?.getColumn('email')?.setFilterValue([])
+}
+
+onMounted(() => {
+  // применяем фильтры при монтировании компонента
+  table?.value?.tableApi?.setColumnFilters(columnFilters.value)
+})
 
 </script>
 
@@ -75,15 +97,25 @@ const columnFilters = ref([
       <USelect
           :model-value="table?.tableApi?.getColumn('status')?.getFilterValue() as Status | undefined"
           :items="statuses"
+          :placeholder="'Выберите статус...'"
           @update:model-value="table?.tableApi?.getColumn('status')?.setFilterValue($event)"
-          class="w-48 capitalize" />
+      />
+
+      <USelect
+          :model-value="table?.tableApi?.getColumn('email')?.getFilterValue() as string[] | undefined"
+          :items="emails"
+          :placeholder="'Выберите один или несколько e-mail...'"
+          multiple
+          @update:model-value="($event) => table?.tableApi?.getColumn('email')?.setFilterValue($event || [])"
+          class="w-2xl"
+      />
 
       <UButton
-          @click="table?.tableApi?.getColumn('status')?.setFilterValue('')"
+          @click="clearFilters"
           :variant="'ghost'"
       >Очистить</UButton>
     </div>
 
-    <UTable ref="table" :data="users" :columns="columns" v-model:column-filters="columnFilters"/>
+    <UTable ref="table" :data="users" :columns="columns" :column-filters="columnFilters"/>
   </div>
 </template>
